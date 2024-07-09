21c902999fee9d2884128455423e741e45b78778
Calf <calf.zhou@gmail.com>
2024-07-09 14:32:36 +0800

    refactor: sort tailwind css classes via Tailwind Raw Reorder

--- src/views/DemoView.tsx
+++ src/views/DemoView.tsx
@@ -6,7 +6,7 @@ interface DemoViewProps {
 
 const DemoView: React.FC<DemoViewProps> = ({ message }) => {
   return (
-    <div className="border-solid border-indigo-600 w-96 bg-slate-200 p-2">
+    <div className="w-96 border-solid border-indigo-600 bg-slate-200 p-2">
       <h1 className="underline">Demo View</h1>
       <p className="text-blue-600">{message}</p>
     </div>
